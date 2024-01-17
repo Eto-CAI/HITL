b50ee5167a2c9dd51486a2988a2121fd77fb6c15
Etoh-CAI <hitohiro.eto.ba@hitachi.com>
2024-01-17 09:17:57 +0900

    M:N,員数複数対応

--- HITL_1.py
+++ HITL_1.py
@@ -8,6 +8,17 @@
 #		SCOEngineController.dll	エンジン本体
 #       SimData                 エンジン入出力CSV一式
 
+# BOM構成拡張・員数拡張　テスト項目
+# [[A,partsA,1],[B,partsB,1],[C,partsC,1]] 1:1 員数単数〇
+# [[A,partsA,2],[B,partsB,3],[C,partsC,4]] 1:1 員数複数〇
+# [[A,partsA,1],[B,partsA,1],[C,partsC,1]] 1:M 員数単数〇
+# [[A,partsA,1],[B,partsA,2],[C,partsC,1]] 1:M 員数複数〇
+# [[A,partsA,1],[A,partsB,1],[C,partsC,1]] M:1 員数単数〇
+# [[A,partsA,1],[A,partsB,2],[C,partsC,1]] M:1 員数複数〇
+# [[A,partsA,1],[A,partsB,1],[B,partsA,1],[B,partsA,1]] M:N 員数単数〇
+# [[A,partsA,1],[A,partsB,2],[B,partsA,3],[B,partsA,4]] M:N 員数複数〇
+# 
+
 #	使用ライブラリ(必須)
 import pandas as pd
 import datetime
@@ -15,7 +26,7 @@ import datetime
 import SCOEngineController as SCO
 
 # マスタパラメータ
-step = 7 #KPI確認日数
+step = 7 #KPI確認日数]
 comp = ("Market").encode('utf-8') #着目企業
 
 # マスタ読込み->後々使うかどうかは不明
@@ -81,42 +92,33 @@ print(unfulfilled_demand_quantity)
 # 子品目・員数取得
 child_items = pd.merge(unfulfilled_demand_quantity, bom_master)
 child_items.columns=["#ParentItemID","Unfulfilled_Demand_Quantity","ChildItemID","Consumption"]
-print("\n5_child_items")
+print("\n5_with_bom_master")
 print(child_items)
 
 # 親品目ごとに需要非充足量を集計
-child_items["Unfulfilled_Demand_Quantity_sum"] = child_items["Unfulfilled_Demand_Quantity"] * child_items["Consumption"]
-print("\n5_2_child_items")
+child_items["Unfulfilled_Demand_Quantity_with_consumption"] = child_items["Unfulfilled_Demand_Quantity"] * child_items["Consumption"]
+print("\n6_Unfulfilled_Demand_Quantity_with_consumption")
 print(child_items)
 
 # 子品目ごとに需要非充足量を集計
-child_items = child_items.groupby('ChildItemID')["Unfulfilled_Demand_Quantity_sum"].sum()
+child_items = child_items.groupby('ChildItemID')["Unfulfilled_Demand_Quantity_with_consumption"].sum()
 child_items = child_items.reset_index(drop=False)
-print("\n6_1_unfulfilled_demand_quantity_per_child_items")
-print(child_items)
-
-# 員数取得
-# child_items = pd.merge(unfulfilled_demand_quantity_per_child_items, bom_master[["ChildItemID","Consumption"]], on="ChildItemID")
-# child_items = child_items.drop_duplicates()
-# print("\n6_2_child_items_groupby")
-# print(child_items)
-
-# tran_masterをマージし、子品目調達LTを取得
 child_items.columns = ["ItemID", "Unfulfilled_Demand_Quantity_with_consumption"]
-# tran_master.columns = ["CompanyISID","ChildItemID","TransportType","ItemUnitPrice","TransportLT","AllocationRate","TransportUnitCost","TransportLotSize","OrderFixedPeriod","TransportMeanID","ResourceAccomAgentISID"]
-print("\n6_3_child_items_groupby")
+print("\n7_unfulfilled_demand_quantity_per_child_items")
 print(child_items)
-kpi_calc = pd.merge(child_items, tran_master, on="ItemID")
+
+# Tran_masterをマージし、子品目調達LTを取得
+kpi_calc = pd.merge(child_items, tran_master[["ItemID","OrderFixedPeriod"]], on="ItemID")
+print("\n8_with_tran_master")
+print(kpi_calc)
 
 # KPI計算：KPIが低いもの＝対策が必要
 # KPI = 係数 / (需要非充足量 * 員数 * 子品目の調達LT)
 Coef = 1000
 kpi_calc["KPI_score"] = Coef / (kpi_calc["Unfulfilled_Demand_Quantity_with_consumption"] * kpi_calc["OrderFixedPeriod"])
-kpi_calc.loc[kpi_calc["Unfulfilled_Demand_Quantity"] == 0, "KPI_score"] = 1
+kpi_calc.loc[kpi_calc["Unfulfilled_Demand_Quantity_with_consumption"] == 0, "KPI_score"] = 1
 
 # 結果をsortして確認
 kpi_calc = kpi_calc.sort_values(by="KPI_score")
-print("\n7_OrderFixedPeriod")
-print(kpi_calc["OrderFixedPeriod"])
-print("\n8_kpi_calu")
+print("\n9_kpi_calc")
 print(kpi_calc)
